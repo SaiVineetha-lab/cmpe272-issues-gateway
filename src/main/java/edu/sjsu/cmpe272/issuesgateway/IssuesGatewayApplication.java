5e6822392ec33a10f5b79d5642230b6f3377265e
Shravani Naikoti <shravaninaikoti@Shravanis-MacBook-Air.local>
2026-09-13 12:36:56 -0700

    Initialize Spring Boot issues gateway

---
+++ src/main/java/edu/sjsu/cmpe272/issuesgateway/IssuesGatewayApplication.java
@@ -0,0 +1,13 @@
+package edu.sjsu.cmpe272.issuesgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuesGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuesGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/sjsu/cmpe272/issuesgateway/IssuesGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sjsu.cmpe272.issuesgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuesGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
